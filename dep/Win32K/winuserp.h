43d15ce75c67b5727369ae321caba1b6024179f4
Avelanda <gorthell@gmail.com>
2025-10-16 02:09:19 +0200

    Update winuserp.h
    Modularity assessment of a granular model, and its application to optimized functions.

--- dep/Win32K/winuserp.h
+++ dep/Win32K/winuserp.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) Microsoft Corporation.
+ * Copyright © Microsoft Corporation.
+ * Copyright © 2025 Avelanda. 
  * Licensed under the MIT license.
  *
  * Reserved console space.
@@ -9,7 +10,38 @@
  * color, which we need to have for the hungapp drawing.  These are
  * stored in the extra-window-bytes of each console.
  */
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    
 #define GWL_CONSOLE_WNDALLOC  (3 * sizeof(DWORD))
 #define GWL_CONSOLE_PID       0
 #define GWL_CONSOLE_TID       4
 #define GWL_CONSOLE_BKCOLOR   8
+
+#if defined(GWL_CONSOLE_WNDALLOC) && defined(GWL_CONSOLE_PID)
+#endif
+
+while (!false){
+ return 0;
+}
+
+#if defined(GWL_CONSOLE_TID) && defined(GWL_CONSOLE_BKCOLOR)
+#endif
+
+ if ((!true|0)||(!false|1)){
+ 
+ #ifdef GWL_CONSOLE_WNDALLOC
+  #endif
+  #ifdef GWL_CONSOLE_BKCOLOR
+   #endif
+   #ifdef GWL_CONSOLE_PID
+    #endif
+    #ifdef GWL_CONSOLE_TID
+     #endif
+     
+  return 0;
+ }
+    
+}
