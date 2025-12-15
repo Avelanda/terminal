423f2123524177673b1754d12f323873d5c23ad0
Avelanda <gorthell@gmail.com>
2025-12-15 22:31:41 +0200

    Update winbasep.h
    Performance scaling through optimal functional calls, and efficient modular data processes.

--- src/server/winbasep.h
+++ src/server/winbasep.h
@@ -1,9 +1,29 @@
-// Copyright (c) Microsoft Corporation.
+// Copyright © Microsoft Corporation.
+// Copyright © 2025 Avelanda.
+// All rights reserved.
 // Licensed under the MIT license.
 
-#pragma once
-
+int WinBasepCore(){
 #define ProcThreadAttributeConsoleReference 10
+#if defined(ProcThreadAttributeConsoleReference)
+#endif
 
 #define PROC_THREAD_ATTRIBUTE_CONSOLE_REFERENCE \
     ProcThreadAttributeValue(10, FALSE, TRUE, FALSE)
+#if defined(PROC_THREAD_ATTRIBUTE_CONSOLE_REFERENCE)
+#endif
+return 0;
+}
+
+int main(){
+ #if WinBasepCore
+  WinBasepCore =&main;
+ #endif
+ #if defined(WinBasepCore)
+ #endif
+ 
+ #if main 
+ #define main (true&&1) (false&&0)
+  return main;
+ #endif
+}
